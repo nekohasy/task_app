cbfa96d167d6be12259b015d2e8eedbde21f1a8c
nekohasy <63384336+nekohasy@users.noreply.github.com>
2022-04-16 18:33:16 +0900

    add rails-i18n

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,6 @@ class Task < ApplicationRecord
 
   def date_before_start
     return if post_end_at.blank?
-    errors.add(:post_end_at, "は今日以降のものを選択してください") if post_end_at < Date.today
+    errors.add(:post_end_at, "は今日以降の日付で選択してください") if post_end_at < Date.today
   end
 end
